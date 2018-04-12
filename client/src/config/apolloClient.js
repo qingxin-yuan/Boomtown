e8f810475f6f10a1755c6955fc1eccb9145dd1b0
qingxin-yuan <qingxin.yn@gmail.com>
2018-04-12 13:56:56 -0700

    rebuild app.

--- client/src/config/apolloClient.js
+++ client/src/config/apolloClient.js
@@ -3,7 +3,7 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const client = new ApolloClient({
-    link: new HttpLink({ uri: 'http://localhost:36154/graphql' }),
+    link: new HttpLink({ uri: `http://localhost:${process.env.PORT}/graphql` }),
     cache: new InMemoryCache()
 });
 
