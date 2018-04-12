b21361f2e72d4593d7852e5a258049bc23d3c324
qingxin-yuan <qingxin.yn@gmail.com>
2018-04-12 12:05:34 -0700

    Remove extra packages.

--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 
 import itemsReducer from './modules/items';
 import shareReducer from './modules/share';
@@ -9,5 +8,4 @@ export default combineReducers({
     items: itemsReducer,
     share: shareReducer,
     auth: authReducer,
-    form: formReducer
 });
