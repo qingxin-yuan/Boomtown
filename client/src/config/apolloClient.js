bf7823e98523628efc34deb5f650ce6fd8595d83
qingxin-yuan <qingxin.yn@gmail.com>
2018-04-20 13:39:00 -0700

    Change gqlServer port in client.

--- client/src/config/apolloClient.js
+++ client/src/config/apolloClient.js
@@ -2,12 +2,12 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const port = process.env.NODE_ENV === 'production' ? 80 : 5000;
-const host = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PUBLIC_URL : 'localhost';
+const gqlServer =
+    process.env.NODE_ENV === 'production' ? 5000 : window.location.host;
 
 const client = new ApolloClient({
     link: new HttpLink({
-        uri: `http://${host}:${port}/graphql`
+        uri: `http://${gqlServer}/graphql`
     }),
     cache: new InMemoryCache()
 });
