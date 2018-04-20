e61564920ddd212812979270b05ee89cb3e603c7
qingxin-yuan <qingxin.yn@gmail.com>
2018-04-20 14:34:06 -0700

    test for pg connection string.

--- server/config.js
+++ server/config.js
@@ -1,12 +1,12 @@
 module.exports = app =>{
 
   //POSTGRES Configs
-  // app.set('PGUSER', process.env.PGUSER || 'boomtowndb');
-  // app.set('PGPASSWORD', process.env.PGPASSWORD || 'boomtowndb');
-  // app.set('PGDATABASE', process.env.PGDATABASE || 'boomtowndb');
-  // app.set('PGHOST', process.env.PGHOST || 'localhost');
-  // app.set('PG_PORT', process.env.PG_PORT || '5432');
-  app.set('PGCONNECTION',process.env.DATABASE_URL);
+  app.set('PGUSER', process.env.PGUSER || 'boomtowndb');
+  app.set('PGPASSWORD', process.env.PGPASSWORD || 'boomtowndb');
+  app.set('PGDATABASE', process.env.PGDATABASE || 'boomtowndb');
+  app.set('PGHOST', process.env.PGHOST || 'localhost');
+  app.set('PG_PORT', process.env.PG_PORT || '5432');
+  // app.set('PGCONNECTION',process.env.DATABASE_URL);
   //  //graphQL Configs
   // app.set('GQL_PORT', process.env.GQL_PORT || '3002');
   //Express Configs
