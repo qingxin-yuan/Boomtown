1d25cfc59c5439fc1df58c0bb57e037a8c4d51b6
qingxin-yuan <qingxin.yn@gmail.com>
2018-04-20 14:07:25 -0700

    set end to production.

--- client/src/config/apolloClient.js
+++ client/src/config/apolloClient.js
@@ -3,7 +3,7 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const gqlServer =
-    process.env.NODE_ENV === 'production' ? window.location.host : 'localhost:5000';
+    process.env.NODE_ENV === 'production' ? `https://${window.location.host}` : 'http://localhost:5000';
 
 const client = new ApolloClient({
     link: new HttpLink({
